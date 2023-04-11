ca105226cfe1b60a3998e1385c24d9676a940187
mikael-fariaa <129950419+mikael-fariaa@users.noreply.github.com>
2023-04-11 08:18:24 -0300

    Add files via upload

---
+++ MikaelMatheusFranciscoBhaskara/src/mikaelmatheusfranciscobhaskara/MikaelMatheusFranciscoBhaskara.java
@@ -0,0 +1,26 @@
+package mikaelmatheusfranciscobhaskara;
+
+import java.util.Scanner;
+
+public class MikaelMatheusFranciscoBhaskara {
+
+
+    public static void main(String[] args) {
+       Bhaskara2 b1 = new Bhaskara2();
+       b1.setA(1);
+       b1.setB(3);
+       b1.setC(-2);
+       b1.ValorDelta();
+       b1.Valorx1();
+       b1.Valorx2();
+       b1.Status();
+    
+}
+}
+        
+        
+        
+        
+   
+        
+    
